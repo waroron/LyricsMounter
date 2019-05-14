d0603d4df48984fa7e1ce201bf5d3c75ae21a001
AnominP <ex.gonw122@gmail.com>
2019-05-14 23:06:24 +0900

    first commit

---
+++ URLs.py
@@ -0,0 +1,3 @@
+UTANET_TOP = 'https://www.uta-net.com/'
+SEARCH = 'search/'
+
---
+++ Uta-Net_manager.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+from URLs import UTANET_TOP, SEARCH
+from soup_util import get_soup, add_query
+import Levenshtein
+
+
+def search_lyrics_from_query(title, artists=None):
+    q = []
+    a_q = ('Aselect', '2')
+    b_q = ('Bselect', '3')
+    title_q = ('Keyword', title)
+    q.append(a_q)
+    q.append(b_q)
+    q.append(title_q)
+
+    url = add_query(UTANET_TOP + SEARCH, q)
+    soup = get_soup(url)
+
+    result_box = soup.find('div', class_='result_table last')   # 検索結果一覧のボックス
+    result_tbody = result_box.find('tbody')                     # result_box内のtbody
+    result_musics = result_tbody.find_all('tr')
+
+    min_id = False
+    min_dist = 100
+    for music in result_musics:
+        cur_title = music.find('td', class_='side td1').find('a').text
+        cur_artists = music.find('td', class_='td2').find('a').text
+        cur_id = music.find('td', class_='side td1').find('a').get('href')
+
+        dist = Levenshtein.distance(artists, cur_artists) + Levenshtein.distance(title, cur_title)
+        if min_dist > dist:
+            min_id = cur_id
+
+        # print('title: {} \nartist: {} \nmusic id: {}\n'.format(cur_title, cur_artists, cur_id))
+
+    return min_id, min_dist
+
+
+def get_lyrics_from_lyrics_page(url):
+    soup = get_soup(url)
+    lyrics_text = soup.find('div', id='kashi_area').text
+
+    return lyrics_text
+
+
+if __name__ == '__main__':
+    url = search_lyrics_from_query('Tulip')
+    lyrics = get_lyrics_from_lyrics_page(url)
+
+    print(lyrics)
